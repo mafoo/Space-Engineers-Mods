b970e3cd67a2623a79b2e4b3557a3620ce8f3106
Mafoo <github@mafoo.org>
2016-11-05 11:40:14 +0000

    User friendly Build-Blender-textures.ps1
    Updated Build-Blender-textures.ps1 so it is more user friendly
    you can set paths at the top
    you don't need imagemagick

--- Build-Blender-textures.ps1
+++ Build-Blender-textures.ps1
@@ -1,16 +1,19 @@
 ﻿$wshell = New-Object -ComObject Wscript.Shell
-$title = "Space Engineers Build Blender textures v1.0"
+$title = "Space Engineers Build Blender textures v1.1"
 
+#START config options
 
-$imagemagick = Get-ChildItem -Path $env:ProgramW6432 ImageMagick*.*.*
-$im_convert = Get-ChildItem -Path $imagemagick.FullName convert.exe
-if(-not $im_convert)
-{
-    $wshell.Popup("Could not find the imagemagick convert.exe", 0, $title, 0x0)
-    exit 1
-}
-
+# set the following to tell the script where to find/put things
+# Microsoft DirectTex - can be obtained from - https://github.com/Microsoft/DirectXTex/releases
 $texconv_raw = "C:\users\mafoo\bin\texconv.exe"
+# space engineers game directory
+# (p.s. you can run this script twice for two differnt sources to build both the Dx11 and Dx9 textures)
+$game_dir_raw = "C:\Games\Steam\steamapps\common\SpaceEngineers"
+# resource directory - where you would like the output to be kept (what you then tell blender to use)
+$resource_dir_raw = "D:\Space Engineers Source"
+
+#END config options
+
 $texconv = Get-Item -Path $texconv_raw
 if(-not $texconv)
 {
@@ -18,7 +21,6 @@ if(-not $texconv)
     exit 1
 }
 
-$game_dir_raw = "C:\Games\Steam\steamapps\common\SpaceEngineers"
 $game_dir = Get-Item -Path $game_dir_raw -ErrorAction SilentlyContinue
 
 if(-not $game_dir)
@@ -27,7 +29,6 @@ if(-not $game_dir)
     exit 1
 }
 
-$resource_dir_raw = "D:\Space Engineers Source"
 $resource_dir = Get-Item -Path $resource_dir_raw -ErrorAction SilentlyContinue
 
 if(-not $resource_dir)
@@ -37,11 +38,14 @@ if(-not $resource_dir)
 }
 
 Try{
+	$se_exe_dir = $resource_dir.FullName + "\bin64"
+	$se_exe = $resource_dir.FullName + "\bin64\SpaceEngineers.exe"
+	$null = New-Item -ItemType Directory -Path $se_exe_dir -ErrorAction SilentlyContinue
+	$null = New-Item -ItemType File -Path $se_exe
     foreach ($item in Get-ChildItem -Path $game_dir.FullName -Recurse){
 		$rel_path = $item.FullName.Replace($game_dir.FullName, "")
 		$dst_path = $resource_dir.FullName + $rel_path
 		if($item -is [System.IO.DirectoryInfo]){
-			#$rel_path
 			if($rel_path -like "\Content\Textures\*"){
 				"Processing " + $dst_path
 				$null = New-Item -Path $dst_path -ItemType Directory -ErrorAction SilentlyContinue
@@ -57,20 +61,18 @@ Try{
 					$dst_dir = $resource_dir.FullName + $rel_dir
 					"Loc: "+ $item.FullName +", Rel: " + $rel_dir + ", " + $dst_dir
 
-					& $texconv -f BC3_UNORM -ft dds -o $dst_dir $item.FullName
-					#(& $im_convert.FullName -define dds:compression=DXT5 $item.FullName $dst_path) 2> $null
+					& $texconv -y -f BC3_UNORM -ft dds -o $dst_dir $item.FullName
 					if($lastExitCode){
-					exit 1
-					#	"Falling back to copy for $rel_path"
-					#	Copy-Item -Path $item -Destination $dst_path
+						Write-Output "Somethign went wrong with the last convert"
+						exit 1
 					}
+					$name = $dst_dir + "\" + $item.BaseName
+					$uc_name = $name +".DDS"
+					$lc_name = $item.BaseName +".dds"
+					Rename-Item -Path $uc_name -NewName $lc_name -Force
 				}
 			}
 		}
-        #$dst_file = $icon.BaseName
-        #$dst_file = $dst_path.FullName + "\Icons\" + ($dst_file -replace "(.*)\d+",'$1') + ".dds"
-        #$dst_file
-        #& $im_convert.FullName -define dds:compression=dxt5 $icon.FullName $dst_file
     }
 }
 Catch
