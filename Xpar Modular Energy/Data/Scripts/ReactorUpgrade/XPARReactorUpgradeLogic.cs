18cd432f119fc80b33a2c6350e0a9000004c5949
Mafoo <github@mafoo.org>
2020-12-08 16:08:05 +0000

    Added Experimental

---
+++ Xpar Modular Energy/Data/Scripts/ReactorUpgrade/XPARReactorUpgradeLogic.cs
@@ -0,0 +1,44 @@
+﻿using Sandbox.Common.Components;
+using Sandbox.Common.ObjectBuilders;
+using Sandbox.Game.Entities;
+using VRage.Game.ModAPI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReactorUpgrade
+{
+    [MyEntityComponentDescriptor(typeof(MyObjectBuilder_Reactor))]
+    public class XPARReactorUpgradeLogic : MyGameLogicComponent
+    {
+        private IMyReactor m_reactor;
+        private VRage.Game.ModAPI.IMyCubeBlock m_parent;
+        private MyObjectBuilder_EntityBase m_objectBuilder = null;
+
+        public override void Init(MyObjectBuilder_EntityBase objectBuilder)
+        {
+            base.Init(objectBuilder);
+
+            m_reactor = Entity as IMyReactor;
+            m_parent = Entity as VRage.Game.ModAPI.IMyCubeBlock;
+
+            m_parent.UpgradeValues.Add("Power", 0f);
+	    
+            m_objectBuilder = objectBuilder;
+
+            m_parent.OnUpgradeValuesChanged += OnUpgradeValuesChanged;
+        }
+
+        public override MyObjectBuilder_EntityBase GetObjectBuilder(bool copy = false)
+        {
+            return m_objectBuilder;
+        }
+
+        private void OnUpgradeValuesChanged()
+        {
+            m_reactor.PowerOutputMultiplier = m_parent.UpgradeValues["Power"] + 1f;
+        }
+    }
+}
---
+++ Xpar Modular Energy/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Xpar Modular Energ")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Xpar Modular Energ")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("8d1383d0-0e3d-4733-b61b-900f4cbfaeb0")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
