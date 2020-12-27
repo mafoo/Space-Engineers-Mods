a1a57d188d2bbadf2dadf7f3bdfb3b4b730b3d3e
Mafoo <github@mafoo.org>
2020-12-27 19:41:19 +0000

    [Modular Reactors] BugFix
    Add code that can cope with other mods that apply the same logic

--- Modular Reactors/Data/Scripts/ReactorUpgradeLogic.cs
+++ Modular Reactors/Data/Scripts/ReactorUpgradeLogic.cs
@@ -1,40 +1,56 @@
-﻿using Sandbox.Common.ObjectBuilders;
-using Sandbox.ModAPI;
-using VRage.Game.Components;
-using VRage.Game.ModAPI;
-using VRage.ObjectBuilders;
+﻿// ***********************************************************************
+// Assembly         : Modular Reactors
+// Created          : 2020-12-08
+// ***********************************************************************
+// <copyright file="ReactorUpgradeLogic.cs">
+//     Copyright (c) 2020 github@mafoo.org. All rights reserved
+// </copyright>
+// ***********************************************************************
 
 // ReSharper disable once CheckNamespace
+
 namespace ModularReactors
 {
+    using Sandbox.Common.ObjectBuilders;
+    using Sandbox.ModAPI;
+    using VRage.Game.Components;
+    using VRage.Game.ModAPI;
+    using VRage.ObjectBuilders;
+
     [MyEntityComponentDescriptor(typeof(MyObjectBuilder_Reactor), true)]
     public class ReactorUpgradeLogic : MyGameLogicComponent
     {
-        private MyObjectBuilder_EntityBase m_objectBuilder = null;
-        private IMyCubeBlock m_parent;
-        private IMyReactor m_reactor;
+        private MyObjectBuilder_EntityBase ObjectBuilder { get; set; }
+        private IMyCubeBlock Parent { get; set; }
+        private IMyReactor Reactor { get; set; }
 
-        public override MyObjectBuilder_EntityBase GetObjectBuilder(bool copy = false) => this.m_objectBuilder;
+        public override MyObjectBuilder_EntityBase GetObjectBuilder(bool copy = false) => this.ObjectBuilder;
 
         public override void Init(MyObjectBuilder_EntityBase objectBuilder)
         {
             base.Init(objectBuilder);
 
-            this.m_reactor = this.Entity as IMyReactor;
-            this.m_parent = this.Entity as IMyCubeBlock;
-            if (this.m_parent == null)
+            this.Reactor = this.Entity as IMyReactor;
+            this.Parent = this.Entity as IMyCubeBlock;
+            if (this.Parent == null)
             {
                 return;
             }
 
-            this.m_objectBuilder = objectBuilder;
-            this.m_parent.UpgradeValues.Add("Power", 0f);
-            this.m_parent.OnUpgradeValuesChanged += this.OnUpgradeValuesChanged;
+            if (this.Parent.UpgradeValues.ContainsKey("Power"))
+            {
+                //Another mod is already doing our logic, skipping the setup
+                return;
+            }
+
+            this.ObjectBuilder = objectBuilder;
+            this.Parent.UpgradeValues.Add("Power", 0f);
+            this.Parent.OnUpgradeValuesChanged += this.OnUpgradeValuesChanged;
         }
 
         private void OnUpgradeValuesChanged()
         {
-            this.m_reactor.PowerOutputMultiplier = this.m_parent.UpgradeValues["Power"] + 1f;
+            this.Reactor.PowerOutputMultiplier = this.Parent.UpgradeValues["Power"] + 1f;
         }
     }
 }
